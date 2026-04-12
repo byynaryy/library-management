9210e69ce41a36e736fb39ccf4f23ab9b96fad0b
jeff <endeki@gmail.com>
2026-04-12 15:55:20 +0200

    feat: add class Book

---
+++ src/main/java/com/oomph/library/Book.java
@@ -0,0 +1,58 @@
+package com.oomph.library;
+
+public class Book {
+    private final String isbn;
+    private final String title;
+    private final String author;
+    private int totalCopies;
+    private int availableCopies;
+
+    public Book(String isbn, String title, String author) {
+        this.isbn = validate(isbn);
+        this.title = validate(title);
+        this.author = validate(author);
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getTotalCopies() {
+        return totalCopies;
+    }
+
+    public int getAvailableCopies() {
+        return availableCopies;
+    }
+
+    public void setTotalCopies(int totalCopies) {
+        if(totalCopies < 1) {
+            throw new IllegalArgumentException("Total copies can not be less than one");
+        }
+        this.totalCopies = totalCopies;
+    }
+
+    public void setAvailableCopies(int availableCopies) {
+        if(availableCopies < 0) {
+            throw new IllegalArgumentException("Available copies can not be negative");
+        }
+        this.availableCopies = availableCopies;
+    }
+
+    private String validate(String field) {
+        if(field == null || field.isBlank()) {
+            throw new IllegalArgumentException(field + "can not be emty");
+        }
+        return field;
+    }
+
+
+}
