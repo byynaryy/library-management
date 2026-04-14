6e0d51bae360c39c89209ba3b5ac264057a75372
jeff <endeki@gmail.com>
2026-04-14 23:20:31 +0200

    chore: add methods that retrieve, save, and delete books

--- src/main/java/com/oomph/library/repository/IBookRepository.java
+++ src/main/java/com/oomph/library/repository/IBookRepository.java
@@ -1,4 +1,12 @@
 package com.oomph.library.repository;
 
+import com.oomph.library.model.Book;
+
+import java.util.Map;
+
 public interface IBookRepository {
+    void addBook(Book book);
+    Book getBook(String isbn);
+    void removeBook(String isbn);
+    boolean exists(String isbn);
 }
